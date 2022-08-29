42182f9811b40da73f8280a4dfcaa18f1768db78
VASILPC\vasil <vasilkosturski@gmail.com>
2022-08-29 09:16:48 +0300

    Started controller

---
+++ src/KafkaCSharpIntegrationTests/OrdersService/OrdersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrdersService;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrdersController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<string>> Get()
+    {
+        return await Task.FromResult(Ok("hui"));
+    }
+}
---
+++ src/KafkaCSharpIntegrationTests/OrdersService/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OrdersService;
+
+internal class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+        app.MapControllers();
+        await app.RunAsync();
+    }
+}
