604f18fce6d9ea5babb2bf7d9b910bcceb16587e
VASILPC\vasil <vasilkosturski@gmail.com>
2022-09-02 09:23:11 +0300

    Started test project

--- src/KafkaCSharpIntegrationTests/OrdersService.Tests/Test.cs
+++ src/KafkaCSharpIntegrationTests/OrdersService.Tests/Test.cs
@@ -1,6 +1,22 @@
-﻿namespace OrdersService.Tests;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Xunit;
+
+namespace OrdersService.Tests;
 
 public class Test
 {
-    
+    [Fact]
+    public async Task OrdersTest()
+    {
+        var application = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder =>
+            {
+                // ... Configure test services
+            });
+
+        var client = application.CreateClient();
+        //...
+    }
 }
