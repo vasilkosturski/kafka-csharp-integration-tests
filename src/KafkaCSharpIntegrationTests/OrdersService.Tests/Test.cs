4106149039966d59bcf87da4f49eb1fd14308636
VASILPC\vasil <vasilkosturski@gmail.com>
2022-09-05 08:27:49 +0300

    Initial test version

--- src/KafkaCSharpIntegrationTests/OrdersService.Tests/Test.cs
+++ src/KafkaCSharpIntegrationTests/OrdersService.Tests/Test.cs
@@ -5,6 +5,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using Confluent.Kafka;
 using Confluent.Kafka.Admin;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
@@ -28,18 +29,40 @@ public class Test : IClassFixture<WebApplicationFactory<Program>>
         
         await CreateKafkaTopic();
         
-        // Act
-        var request = new Order
+        var order = new Order
         {
             Id = Guid.NewGuid().ToString(),
             Price = 10,
             Product = Product.Shirt
         };
-        var ser = JsonSerializer.Serialize(request);
+        var ser = JsonSerializer.Serialize(order);
+        
+        // Act
+        
         var response = await client.PostAsync(url, new StringContent(ser, Encoding.UTF8, "application/json"));
         
         // Assert
         response.EnsureSuccessStatusCode(); // Status Code 200-299
+        
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = "localhost:9092",
+            GroupId = Guid.NewGuid().ToString(),
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using (var consumer = new ConsumerBuilder<Null, Order>(config)
+                   .SetValueDeserializer(new CustomValueDeserializer<Order>())
+                   .Build())
+        {
+            consumer.Subscribe("orders");
+
+            var res = consumer.Consume(TimeSpan.FromMilliseconds(1));
+
+            res.Message.Value.Id.Should().Be(order.Id);
+            res.Message.Value.Price.Should().Be(order.Price);
+            res.Message.Value.Product.Should().Be(order.Product);
+        }
     }
 
     private static async Task CreateKafkaTopic()
--- src/KafkaCSharpIntegrationTests/OrdersService/OrdersProducerFactory.cs
+++ src/KafkaCSharpIntegrationTests/OrdersService/OrdersProducerFactory.cs
@@ -1,5 +1,4 @@
-﻿using System.Text;
-using System.Text.Json;
+﻿using System.Text.Json;
 using Confluent.Kafka;
 
 namespace OrdersService;
@@ -28,7 +27,24 @@ public class CustomValueSerializer<T> : ISerializer<T>
 {
     public byte[] Serialize(T data, SerializationContext context)
     {
-        return Encoding.UTF8.GetBytes(JsonSerializer.Serialize(data, typeof(T)));
+        using var ms = new MemoryStream();
+        
+        var jsonString = JsonSerializer.Serialize(data);
+        var writer = new StreamWriter(ms);
+
+        writer.Write(jsonString);
+        writer.Flush();
+        ms.Position = 0;
+
+        return ms.ToArray();
+    }
+}
+
+public class CustomValueDeserializer<T> : IDeserializer<T>
+{
+    public T Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
+    {
+        return JsonSerializer.Deserialize<T>(data.ToArray());
     }
 }
 
